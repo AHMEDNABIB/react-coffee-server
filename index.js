ff31221406a691133647a48886e647257774528f
AHMEDNABIB <nabibpallab@gmail.com>
2023-11-14 19:39:55 +0600

    crud delete complete

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const express = require("express");
 const cors = require("cors");
@@ -46,6 +46,13 @@ async function run() {
 			res.send(result)
 
 	   })
+
+		app.delete('/coffee/:id', async (req, res) => {
+			const id = req.params.id;
+			const query = { _id: new ObjectId(id) }
+			const result = await coffeeCollection.deleteOne(query)
+			res.send(result)
+		})
 
 
 		// Send a ping to confirm a successful connection
