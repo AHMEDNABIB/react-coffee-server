7a60b095d9d760ecb27a13f756a5b3c41471bf0d
AHMEDNABIB <nabibpallab@gmail.com>
2023-11-14 19:49:37 +0600

    crud find for update is complete

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dlgosdc.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,30 +28,34 @@ async function run() {
 		// Connect the client to the server	(optional starting in v4.7)
 		await client.connect();
 
-		const coffeeCollection= client.db('coffeeDB').collection('coffee')
-        
-		app.get('/coffee', async (req, res) => {
-			const cursor = coffeeCollection.find()
-			const result = await cursor.toArray()
-			res.send(result)
-		})
+		const coffeeCollection = client.db("coffeeDB").collection("coffee");
 
-		app.post('/coffee', async (req, res) => {
+		app.get("/coffee", async (req, res) => {
+			const cursor = coffeeCollection.find();
+			const result = await cursor.toArray();
+			res.send(result);
+		});
+
+		app.get("/coffee/:id", async (req, res) => {
+			const cursor = coffeeCollection.find();
+			const result = await cursor.toArray();
+			res.send(result);
+		});
+
+		app.post("/coffee", async (req, res) => {
 			const newCoffee = req.body;
-			console.log(newCoffee)
+			console.log(newCoffee);
 
-			const result = await coffeeCollection.insertOne(newCoffee)
-			res.send(result)
+			const result = await coffeeCollection.insertOne(newCoffee);
+			res.send(result);
+		});
 
-	   })
-
-		app.delete('/coffee/:id', async (req, res) => {
+		app.delete("/coffee/:id", async (req, res) => {
 			const id = req.params.id;
-			const query = { _id: new ObjectId(id) }
-			const result = await coffeeCollection.deleteOne(query)
-			res.send(result)
-		})
-
+			const query = { _id: new ObjectId(id) };
+			const result = await coffeeCollection.deleteOne(query);
+			res.send(result);
+		});
 
 		// Send a ping to confirm a successful connection
 		await client.db("admin").command({ ping: 1 });
